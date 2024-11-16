5ebf94b44af11d450933003dc10b7bbb17cfcbb8
Pedro Lemes <113155395+code-pedrolsl@users.noreply.github.com>
2024-11-15 21:55:19 -0300

    Update fase_4.py

--- hermit_offboard/fase_4.py
+++ hermit_offboard/fase_4.py
@@ -6,6 +6,11 @@ from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
 from std_msgs.msg import String
 
+'''
+    Precisa conferir como usar o takeoff e o land, eles usam a posição do
+    drone e o drone_controller.py está usando velocity ao invés de position.
+'''
+
 class MissionTaskManager(Node):
     def __init__(self, drone_controller):
         super().__init__('mission_task_manager')
